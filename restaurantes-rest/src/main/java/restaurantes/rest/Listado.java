27e298efddda3ff5d467277f6a1dd7f71156d1c6
Gonzalo <gonzalo.manzanaresc@um.es>
2023-03-12 21:38:36 +0100

    restaurantes rest

---
+++ restaurantes-rest/src/main/java/restaurantes/rest/Listado.java
@@ -0,0 +1,41 @@
+package restaurantes.rest;
+
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+import restaurantes.servicio.RestauranteResumen;
+
+@XmlRootElement
+public class Listado {
+
+	public static class ResumenExtendido {
+		
+		private String url;
+		private RestauranteResumen resumen;
+		
+		public String getUrl() {
+			return url;
+		}
+		public void setUrl(String url) {
+			this.url = url;
+		}
+		public RestauranteResumen getResumen() {
+			return resumen;
+		}
+		public void setResumen(RestauranteResumen resumen) {
+			this.resumen = resumen;
+		}
+		
+	}
+	
+	private List<ResumenExtendido> restaurante;
+	
+	public List<ResumenExtendido> getRestaurante() {
+		return restaurante;
+	}
+	
+	public void setRestaurante(List<ResumenExtendido> restaurante) {
+		this.restaurante = restaurante;
+	}
+}
---
+++ restaurantes-rest/src/main/java/restaurantes/rest/RestaurantesControladorRest.java
@@ -0,0 +1,172 @@
+package restaurantes.rest;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.xml.sax.SAXException;
+
+import io.swagger.annotations.Api;
+import repositorio.EntidadNoEncontrada;
+import repositorio.RepositorioException;
+import restaurantes.modelo.Plato;
+import restaurantes.modelo.Restaurante;
+import restaurantes.modelo.SitioTuristico;
+import restaurantes.rest.Listado.ResumenExtendido;
+import restaurantes.servicio.IServicioRestaurante;
+import restaurantes.servicio.RestauranteResumen;
+import servicio.FactoriaServicios;
+
+@Api
+@Path("restaurantes")
+public class RestaurantesControladorRest {
+	
+	private IServicioRestaurante servicio=FactoriaServicios.getServicio(IServicioRestaurante.class);
+	@Context
+	private UriInfo uriInfo;
+	
+	
+	//1.String create(Restaurante restaurante);
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response create(Restaurante restaurante) throws Exception {
+		System.out.println(restaurante.getNombre());
+		System.out.println(restaurante.getCp());
+		String id= servicio.create(restaurante);
+		System.out.println(id);
+		URI nuevaURL= uriInfo.getAbsolutePathBuilder().path(id).build();
+		return Response.created(nuevaURL).build();
+	}
+	//2.void update(Restaurante restaurante);
+	
+	@PUT
+	@Path("/{id}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response update(@PathParam("id") String id, Restaurante restaurante) throws Exception
+	{
+		if(!id.equals(restaurante.getId()))
+			throw new IllegalArgumentException("El identificador no coincide: "+id);
+		
+		servicio.update(restaurante);
+		return Response.status(Response.Status.NO_CONTENT).build();
+	
+	}
+	
+	//3.List<SitioTuristico> obtenerSitiosTuristicos(String idRes) throws MalformedURLException, SAXException, IOException, ParserConfigurationException;
+	@GET
+	@Path("/{id}/sitios")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response obtenerSitiosTuristicos(@PathParam("id") String id) throws MalformedURLException, SAXException, IOException, ParserConfigurationException, RepositorioException, EntidadNoEncontrada {
+		
+		List<SitioTuristico> resultado= servicio.obtenerSitiosTuristicos(id);
+		return Response.ok(resultado).build();
+		
+		
+	}
+	
+	//4.void setSitiosTuristicos(String id, List<SitioTuristico> sitios);
+	@PUT
+	@Path("/{id}/sitios")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response setSitiosTuristicos(@PathParam("id") String id, List<SitioTuristico> sitios) throws RepositorioException, EntidadNoEncontrada {
+		
+		servicio.setSitiosTuristicos(id, sitios);
+		return Response.status(Response.Status.NO_CONTENT).build();
+		
+	}
+	
+	//5.void addPlato(String idRes, Plato plato);
+	@POST
+	@Path("/{id}/platos")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response addPlato(@PathParam("id") String id, Plato plato) throws RepositorioException, EntidadNoEncontrada {
+		
+		String nombre= servicio.addPlato(id, plato);
+		URI nuevaURL = uriInfo.getAbsolutePathBuilder().path(nombre).build();
+		
+		return Response.created(nuevaURL).build();
+	}
+
+	//6.void removePlato(String idRes, String nombrePlato);
+	@DELETE
+	@Path("/{id}/platos/{nombrePlato}")
+	public Response removePlato(@PathParam("id") String id, @PathParam("nombrePlato") String nombrePlato) throws RepositorioException, EntidadNoEncontrada{
+		
+		servicio.removePlato(id,nombrePlato);
+		return Response.status(Response.Status.NO_CONTENT).build();
+	}
+	
+	//7.void updatePlato(String idRes, Plato plato);
+	@PUT
+	@Path("/{id}/platos/")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response updatePlato(@PathParam("id") String id, Plato plato) throws RepositorioException, EntidadNoEncontrada {
+		
+		servicio.updatePlato(id, plato);
+		return Response.status(Response.Status.NO_CONTENT).build();
+		
+	}
+	
+	//8.void deleteRestaurante(String idRes);
+	@DELETE
+	@Path("/{id}")
+	public Response deleteRestaurante(@PathParam("id") String id) throws RepositorioException, EntidadNoEncontrada{
+		
+		servicio.deleteRestaurante(id);
+		return Response.status(Response.Status.NO_CONTENT).build();
+	}
+	
+	//9.List<RestauranteResumen> getListadoRestaurantes();
+	@GET
+	@Produces({MediaType.APPLICATION_JSON })
+	public Response getListadoActividades() throws Exception {
+
+		List<RestauranteResumen> resultado = servicio.getListadoRestaurantes();
+
+		LinkedList<ResumenExtendido> extendido = new LinkedList<Listado.ResumenExtendido>();
+
+		for (RestauranteResumen restauranteResumen : resultado) {
+
+			ResumenExtendido resumenExtendido = new ResumenExtendido();
+
+			resumenExtendido.setResumen(restauranteResumen);
+
+			// URL
+
+			String id = restauranteResumen.getId();
+			URI nuevaURL = uriInfo.getAbsolutePathBuilder().path(id).build();
+
+			resumenExtendido.setUrl(nuevaURL.toString()); // string
+
+			extendido.add(resumenExtendido);
+
+		}
+
+		// Una lista no es un documento válido en XML
+
+		// Creamos un documento con un envoltorio
+
+		Listado listado = new Listado();
+
+		listado.setRestaurante(extendido);
+
+		return Response.ok(listado).build();
+
+	}
+}
