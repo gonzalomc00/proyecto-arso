2c332a5fe6d82544576567d35212faa06912eb15
Gonzalo <gonzalo.manzanaresc@um.es>
2023-04-06 13:31:18 +0200

    GraphQL pero no funciona con Mongo

--- opinion-graphql/src/main/java/opiniones/graphql/Mutation.java
+++ opinion-graphql/src/main/java/opiniones/graphql/Mutation.java
@@ -12,7 +12,6 @@ public class Mutation implements GraphQLRootResolver {
 	private IServicioOpinion servicio = FactoriaServicios.getServicio(IServicioOpinion.class);
 	
     public String crearOpinion(String nombre) throws RepositorioException {
-        
     	return servicio.createOpinion(nombre);
     }
 }
